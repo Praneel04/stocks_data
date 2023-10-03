5464975f54625c0a50f2390ce863159d192d6f95
Praneel04 <praneelpatel04@gmail.com>
2023-10-03 22:39:09 +0530

    The prices column is added.Will work on signals tomorrow

--- scrape.py
+++ scrape.py
@@ -1,2 +1,21 @@
-import requests
-import BeautifulSoup as bs4
+import requests 
+import pandas as pd
+from bs4 import BeautifulSoup 
+url="https://www.moneycontrol.com/india/stockpricequote/diversified/itc/ITC"
+page=requests.get(url)
+print(page)
+df=pd.read_csv("./stocks1.csv")
+print(df)
+soup=BeautifulSoup(page.content,'html.parser')
+price = soup.find('div', class_ ='inprice1 nsecp').text
+print(price)
+df1=pd.DataFrame()
+df1['companies']=df['companies']
+df1['prices']=''
+print(df1)
+for i, company_url in enumerate(df['url']):
+    page = requests.get(company_url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    price = soup.find('div', class_='inprice1 nsecp').text
+    df1.at[i, 'prices'] = price  # Add the price to the corresponding row
+print(df1)
